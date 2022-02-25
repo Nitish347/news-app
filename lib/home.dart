d0ea52346e84a6f4114165652bef53b0e3b2ca94
Nitish347 <nitishchaurasiya3470@gmail.com>
2022-02-25 13:47:51 +0530

    countries added

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,5 @@
-
 import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:webview_flutter/webview_flutter.dart';
@@ -14,19 +14,70 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List<NewsModel> newsList = new List<NewsModel>();
   bool _loading = false;
+  String query;
+  List<String> countryList = [
+    'Australia',
+    'Belgium',
+    'Brazil',
+    'Canada',
+    'China',
+    'France',
+    'Germany',
+    'India',
+    'Indonesia',
+    'Ireland',
+    'Israel',
+    'Japan',
+    'New Zealand',
+    'Russia',
+    'Saudi Arabia',
+    'Singapore',
+    'South Africa',
+    'South korea',
+    'Switzerland',
+    'UAE',
+    'United Kingdom',
+    'Ukrain',
+    'United State'
+  ];
+  List<String> countryCode = [
+    'au',
+    'be',
+    'br',
+    'ca',
+    'cn',
+    'fr',
+    'de',
+    'in',
+    'id',
+    'ie',
+    'il',
+    'jp',
+    'nz',
+    'ru',
+    'sa',
+    'sg',
+    'za',
+    'kr',
+    'ch',
+    'ae',
+    'ua',
+    'gb',
+    'us'
+  ];
   @override
   void initState() {
-    getNews();
+    getNews('in');
     super.initState();
   }
 
-  getNews() async {
+  getNews(String query) async {
     setState(() {
       _loading = true;
     });
     newsList = new List();
     String api_url =
-        "https://newsapi.org/v2/top-headlines?country=in&apiKey=2b826ea9d85542a898f1ef683de48b71";
+        "https://newsapi.org/v2/top-headlines?country=$query&apiKey=2b826ea9d85542a898f1ef683de48b71";
     var response = await http.get(Uri.parse(api_url));
 
     Map<String, dynamic> jsonData = json.decode(response.body);
@@ -51,37 +102,76 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+          child: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Colors.teal.withOpacity(0.4),
+          Colors.teal,
+        ])),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height / 6,
+                color: Colors.teal,
+                child: Center(
+                  child: Text(
+                    "Select Country",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                physics: ClampingScrollPhysics(),
+                children: List.generate(countryList.length, (index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                        getNews(countryCode[index]);
+                      },
+                      child: ListTile(
+                        leading: Text(
+                          countryList[index],
+                          style: TextStyle(color: Colors.black, fontSize: 20),
+                        ),
+                        trailing: Icon(
+                          CupertinoIcons.search,
+                          size: 30,
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+              )
+            ],
+          ),
+        ),
+      )),
       appBar: AppBar(
         shadowColor: Colors.green,
         backgroundColor: Colors.teal,
-        title: Center(child: Text("Breaking News",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),)),
+        title: Center(
+            child: Text(
+          "Breaking News",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        )),
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            // Container(
-            //   decoration: BoxDecoration(
-            //     color: Colors.teal,
-            //   ),
-            //   height: MediaQuery.of(context).size.height / 6,
-            //   width: MediaQuery.of(context).size.width,
-            //   child: Center(
-            //     child: Text(
-            //       "Breaking News",
-            //       style: TextStyle(
-            //           shadows: [
-            //             Shadow(
-            //               // blurRadius: 10.0,
-            //               offset: Offset(5.0, 5.0),
-            //             ),
-            //           ],
-            //           fontSize: 40,
-            //           color: Colors.white,
-            //           fontWeight: FontWeight.bold,
-            //           fontFamily: 'Raleway'),
-            //     ),
-            //   ),
-            // ),
             SizedBox(
               height: 20,
             ),
@@ -91,10 +181,6 @@ class _HomeState extends State<Home> {
                     color: Colors.black,
                   ))
                 : Container(
-                    decoration: BoxDecoration(
-                        // borderRadius: BorderRadius.circular(40),
-                        // color: Colors.teal
-                        ),
                     child: ListView(
                       shrinkWrap: true,
                       scrollDirection: Axis.vertical,
@@ -133,8 +219,8 @@ class NewsTile extends StatelessWidget {
                 context,
                 MaterialPageRoute(
                     builder: (context) => NewsPage(
-                    newsUrl: url,
-                    )));
+                          newsUrl: url,
+                        )));
           },
           child: Container(
             child: Column(
@@ -196,8 +282,6 @@ class NewsTile extends StatelessWidget {
 class NewsPage extends StatefulWidget {
   final String newsUrl;
   NewsPage({this.newsUrl});
-
-
   @override
   State<NewsPage> createState() => _NewsPageState();
 }
@@ -214,23 +298,20 @@ class _NewsPageState extends State<NewsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.teal,
-          title: Text(
-            "News",
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          ),
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text(
+          "News",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
         ),
-        body: Container(
-          // padding: EdgeInsets.only(top: 30),
-          child: WebView(
-            javascriptMode: JavascriptMode.unrestricted,
-            initialUrl: widget.newsUrl,
-          ),
+      ),
+      body: Container(
+        // padding: EdgeInsets.only(top: 30),
+        child: WebView(
+          javascriptMode: JavascriptMode.unrestricted,
+          initialUrl: widget.newsUrl,
         ),
-
+      ),
     );
   }
 }
-
-
